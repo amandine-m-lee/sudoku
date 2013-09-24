896cbd163da7c444a972f05735feb47cb017c84e
Amandine Lee <amandine.m.lee@gmail.com>
2013-09-24 16:44:17 -0700

    First pass at print method

--- sudoku_board.py
+++ sudoku_board.py
@@ -7,11 +7,30 @@ class SudokuUniquenessException(Exception):
 class Board(object):
 
     def __init__(self):
+
+        self.cols = [Column(i) for i in xrange(9)]
+        self.rows = [Row(i) for i in xrange(9)]
+        self.boxes = [Box(i) for i in xrange(9)]
+
+
         pass
 
+    def print_board(self):
+        print('-'*20)
+        colnum = 0
+        for _ in xrange(3):
+            for _ in xrange(3):
+                subrows = [join(self.cols[colnum].nums[i:i+3]) for i in range(3)]
+                print('|' + '|'.join(subrows) + '|')
+                colnum += 1 
+
+            print('-'*20)
+
+
+
 class Unit(object):
-    def __init__(self, start_nums=[]):
-        self.nums = set(nums)
+    def __init__(self, start_nums):
+        self.nums = start_nums
 
     def free(num):
         return num not in self.nums
@@ -22,30 +41,29 @@ class Unit(object):
 
 class Box(Unit):
 
-    def __init__(self, index, start_nums):
+    def __init__(self, index, start_nums=[0 for _ in xrange(9)]):
         super().__init__(start_nums)
         self.index = index
 
 class Row(Unit):
     
-    def __init__(self, rownum, start_nums):
+    def __init__(self, rownum, start_nums=[0 for _ in xrange(9)]):
         super().__init__(start_nums)
         self.index = rownum #Needs to be within 0 and 8
 
 class Column(Unit):
 
-    def __init__(self, colnum, start_nums):
+    def __init__(self, colnum, start_nums=[0 for _ in xrange(9)]):
         super().__init__(start_nums)
         self.index = colnum #Needs to be between 0 and 8
 
-"""Questions:
+"""Questions/Thoughts:
     - Should I make my own error for when a number does not work?
     - Start with user solving, add AI later
     - How exactly does inheritance work?
     - Put in safeguards to make sure the indexy attributes are in the correct range
-    
-    Thoughts:
-    - Do this in Python 3?"""
+    - Do this in Python 3?
+    - Refactor where type is just a member of unit? Really they are the same. maybe I'll went different print methods though"""
 
 
 
