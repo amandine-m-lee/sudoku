ca83672418dd2f84bc3eb4b63f200718a36f154c
Amandine Lee <amandine.m.lee@gmail.com>
2013-09-25 00:11:27 -0700

    Refactor units as pure sets, use only row and col

--- sudoku_board.py
+++ sudoku_board.py
@@ -2,78 +2,117 @@
 #For consistency, everything with be 1 index
 #Some other design thoughts -
 
-class SudokuUniquenessException(Exception):
+class SudokuUniquenessError(Exception):
     pass
+
+class SudokuValueError(Exception):
+    pass 
+
+class SudokuIndexError(Exception):
+    pass
+
 class Board(object):
 
-    def __init__(self):
-
+    def __init__(self, start_board=None): #start_board should be a 9x9 array with 0 as a placeholder
+        self.grid = [[0 for _ in range(9)] for _ in range(9)]
         self.cols = [Column(i) for i in range(9)]
         self.rows = [Row(i) for i in range(9)]
-        self.boxes = [Box(i) for i in range(9)]
+        self.boxes = [[Box(i, j) for i in range(3)] for j in range(3)]
+
+        if start_board is not None: #Could refactor this probably. 
+            for x in range(9):
+                for y in range(9):
+                    n = start_board[x][y]
+                    if n != 0:
+                        self.add_number(n, x, y)
 
 
-        pass
-
-    def print_board(self):
-        out = ('-'*19)
-        colnum = 0
+    """def print_board(self):
+        print('-'*19)
+        rownum = 0
         for _ in range(3):
             for _ in range(3):
-                subrows = [' '.join(map(str, self.cols[colnum].nums[i:i+3])) for i in range(3)]
+                subrows = [' '.join(map(str, self.row[rownum].nums[i:i+3])) \
+                        for i in range(3)]
                 print('|' + '|'.join(subrows) + '|')
-                colnum += 1 
+                rownum += 1 
 
-            print('-'*19)
+            print('-'*19)"""
 
+
+    def add_number(self, num, row, col):
+     
+        if num not in range(1,10):
+            raise SudokuValueError
+        elif row not in range(9) or col not in range(9):
+            raise SudokuIndexError
+        else:
+            self.grid[row][col] = num
+            self.cols[col].add(num)
+            self.rows[row].add(num)
+            self.boxes[row//3][col//3].add(num)
+
+    def remove_number(self, row, col):
+        num = self.grid[row][col]
+        if num != 0:#throw excpetion if it's already 0?
+            self.cols[col].remove(num)
+            self.rows[row].remove(num)
+            self.boxes[row//3][col//3].remove(num)
+            self.gric
+        return num # I like the idea of returning it like pop 
 
 
 class Unit(object):
-    def __init__(self, start_nums):
-        self.nums = start_nums
-
+    def __init__(self, ind, start_nums):
+        self.nums = set(start_nums)
+        self.index = ind
 
     def free(num):
         return num not in self.nums
 
     def add(num):
         if num in self.nums:
-            raise SudokuUniquenessException("{} not unique".format(num)) 
+            raise SudokuUniquenessError("{} not unique".format(num)) 
+        else:
+            self.nums.add(num)
+
+    def remove(self, num):
+        self.nums.remove(num)
 
 class Box(Unit):
 
-    def __init__(self, index, start_nums=[0 for _ in range(9)]):
-        super().__init__(start_nums)
-        self.index = index
+    def __init__(self, boxrow, boxcol, start_nums=[]):
+        super().__init__((boxrow, boxcol), start_nums)
 
     def __repr__(self):
-        return '\n'.join([' '.join(map(str, self.nums[i:i+3])) for i in range(3)])
+        return "Box {} containing {}".format(self.index, self.nums)
 
 class Row(Unit):
     
-    def __init__(self, rownum, start_nums=[0 for _ in range(9)]):
-        super().__init__(start_nums)
-        self.index = rownum #Needs to be within 0 and 8
+    def __init__(self, rownum, start_nums=[]):
+        super().__init__(start_nums, rownum)
 
     def __repr__(self):
-        return ' '.join(map(str, self.nums))
+        return "Row {} containing {}".format(self.index, self.nums)
 
 class Column(Unit):
 
-    def __init__(self, colnum, start_nums=[0 for _ in range(9)]):
+    def __init__(self, colnum, start_nums=[]):
         super().__init__(start_nums)
-        self.index = colnum #Needs to be between 0 and 8
 
     def __repr__(self):
-        return '\n'.join(map(str, self.nums))
+        return "Column {} containing {}".format(self.nums)
 
 """Questions/Thoughts:
     - Should I make my own error for when a number does not work?
     - Start with user solving, add AI later
     - How exactly does inheritance work?
-    - Put in safeguards to make sure the indexy attributes are in the correct range
+    - Put in safeguards to make sure the indexy attributes are in the correct range (0 to 8 or 1 to 9...?)
     - Do this in Python 3?
-    - Refactor where type is just a member of unit? Really they are the same. maybe I'll went different print methods though"""
+    - Refactor where type is just a member of unit? Really they are the same. maybe I'll went different print methods though
+    - Generate my own sudoku boards?
+    - I could keep a 9x9 simple list to hold the numbers... or I could rely on Column and Row being implemented 
+      correctly, and just use one of them. Let's start with """
 
 
 
