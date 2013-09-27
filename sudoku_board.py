889a38501a264bbd7d333c1362ae87988e1090fd
Amandine Lee <amandine.m.lee@gmail.com>
2013-09-27 00:00:29 -0700

    Change to one-indexing for rows and columns, possibly against my better judgement

--- sudoku_board.py
+++ sudoku_board.py
@@ -15,17 +15,17 @@ class Board(object):
 
     def __init__(self, start_board=None): #start_board should be a 9x9 array with 0 as a placeholder
         self.grid = [[0 for _ in range(9)] for _ in range(9)]
-        self.cols = [Column(i) for i in range(9)]
-        self.rows = [Row(i) for i in range(9)]
-        self.boxes = [[Box(i, j) for i in range(3)] for j in range(3)]
-        self.possible = [[set(range(1,10)) for _ in range(9)] for _ in range(10)]
+        self.cols = [Column(i) for i in range(1, 10)]
+        self.rows = [Row(i) for i in range(1, 10)]
+        self.boxes = [[Box(i, j) for i in range(1,4)] for j in range(1,4)]
+        self.possible = [[set(range(1,10)) for _ in range(9)] for _ in range(9)]
 
         if start_board is not None: #Could refactor this probably. 
             for x in range(9):
                 for y in range(9):
                     n = start_board[x][y]
                     if n != 0:
-                        self.add_number(n, x, y)
+                        self.add_number(n, x+1, y+1)
 
 
     def print_board(self):
@@ -41,8 +41,10 @@ class Board(object):
             print('-'*19)
 
 
-    def add_number(self, num, row, col):
-     
+    def add_number(self, num, row_label, col_label):
+        row = row_label - 1
+        col = col_label - 1
+
         if num not in range(1,10):
             raise SudokuValueError
         elif row not in range(9) or col not in range(9):
@@ -55,7 +57,10 @@ class Board(object):
             self.possible[x][y] = set(n) #Doesn't necessarily have to be a set. 
 
 
-    def remove_number(self, row, col):
+    def remove_number(self, row_label, col_label):
+        row = row_label - 1
+        col = col_label - 1
+
         num = self.grid[row][col]
         if num != 0:#throw excpetion if it's already 0?
             self.cols[col].remove(num)
@@ -64,11 +69,17 @@ class Board(object):
             self.grid[row][col] = 0
         return num # I like the idea of returning it like pop 
 
-    def valid_play(self, num, row, col):
+    def valid_play(self, num, row_label, col_label):
+        row = row_label - 1
+        col = col_label - 1
+
         return self.cols[col].free(num) and self.row[row].free(num) \
                 and self.boxes[row//3][col//3].free(num)
 
-    def deem_impossible(self, num, row, col):
+    def deem_impossible(self, num, row_label, col_label):
+        row = row_label - 1
+        col = col_label - 1
+
         self.possible[row][col].remove(num)
 
 
@@ -120,7 +131,7 @@ class Column(Unit):
     - Should I make my own error for when a number does not work?
     - Start with user solving, add AI later
     - How exactly does inheritance work?
-    - Put in safeguards to make sure the indexy attributes are in the correct range (0 to 8 or 1 to 9...?)
+    - Put in safeguards to make sure the indexy attributes are in the correct range (0 to 8 or 1 to 9...?) <- using it with 0 to 9 is a little difficult. 
     - Do this in Python 3?
     - Refactor where type is just a member of unit? Really they are the same. maybe I'll went different print methods though
     - Generate my own sudoku boards?
