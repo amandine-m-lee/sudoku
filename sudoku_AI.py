fa104430dc62dde355ca1955ae065c00ef1ced08
Amandine Lee <amandine.m.lee@gmail.com>
2013-10-01 21:56:31 -0700

    Some debugging; refactor decrement and increment as list of open cells

--- sudoku_AI.py
+++ sudoku_AI.py
@@ -10,31 +10,33 @@ class DFSPlayer(object):
     def __init__(self, board):
         self.board = board
         self.plays = [] #This can actually act like a log now
-        self.cur_row = 1
-        self.cur_col = 1
-        self.tracker = [self.poss(1,1)] #our stack of generators!
+        self.tracker = [self.poss(1,1,self.board)] #our stack of generators!
+        self.open_cells = self.get_open_cells(self.board)
+        self.cell_index = 0
+        self.cur_row, self.cur_col = self.open_cells[0]
 
     def next_play(self):
-    
+        self.board.print_board()   
         try:
             num = next(self.tracker[-1])
-            self.board.add(num, self.cur_row, self.cur_col)
+            self.board.add_number(num, self.cur_row, self.cur_col)
             self.plays.append((num, self.cur_row, self.cur_col))
             self.increment_coord()
-            self.tracker.append(self.poss(self.cur_row, self.cur_col))
-            
+            self.tracker.append(self.poss(self.cur_row, self.cur_col, self.board))
+            return num 
+
         except StopIteration:
             self.tracker.pop()
             self.decrement_coord()
-            self.board.remove(self.cur_row, self.cur_col)
+            self.board.remove_number(self.cur_row, self.cur_col)
             self.next_play()
 
-    def solve_board():
+    def solve_board(self):
         while(True):
             try:
                 self.next_play()
             except BoardSolved:
-                print("BOARD IS SOLVED")
+                print("BOARD IS SOLVED in {} steps".format(len(self.plays)))
                 self.board.print_board()
                 break
             except NoSolutions:
@@ -42,33 +44,32 @@ class DFSPlayer(object):
                 break
 
     def increment_coord(self): #This might be better refactored as a list with an index that scrolls back and forth. 
-        if self.cur_col < 10:
-            self.cur_col += 1
-        else:
-            if self.cur_row < 10:
-                self.cur_col = 1
-                self.cur_row += 1
-            else:
-                raise BoardSolved
-        if not self.board.removable(self.cur_row - 1, self.cur_col -1):
-            self.increment_coord()#Should probably return or something here
-    
+        self.cell_index += 1
+        try:
+            self.cur_row, self.cur_col = self.open_cells[self.cell_index]
+        except IndexError:
+            raise BoardSolved
+
     def decrement_coord(self):
-        if self.cur_col != 1:
-            self.cur_col -= 1
+        self.cell_index -= 1
+        if self.cell_index > -1:
+            self.cur_row, self.cur_col = self.open_cells[self.cell_index]
         else:
-            if self.cur_row == 1:
-                raise NoSolutions
-            else:
-                self.cur_row -= 1
-                self.cur_col = 9
-        if not self.board.removable(self.cur_row - 1, self.cur_col -1):
-            self.decrement_coord() 
-            
-    def poss(self, row, col): # A generator. Will i run into problems as i mutate stae?
+            raise NoSolutions
+
+    def poss(self, row, col, board): # A generator. Will i run into problems as i mutate stae?
         for i in range(1,10):
-            if self.board.valid_play(i, row, col):
+            if board.valid_play(i, row, col):
                 yield i
+
+    def get_open_cells(self, board):
+        ret = []
+        for i in range(9):
+            for j in range(9):
+                if board.removable[i][j]:
+                    ret.append((i+1, j+1))
+        return ret
+
 
     #If it does't have nxt, then wahta/ 
 
@@ -76,9 +77,8 @@ if __name__ == '__main__':
     from sample_boards import b1
 
     pl = DFSPlayer(b1)
+    pl.solve_board()
 
-
-        
 
 '''Thoughts here:
     - Pass it a board object? Or something else?
