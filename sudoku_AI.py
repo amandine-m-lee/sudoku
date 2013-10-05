81457429f140d7df34f1a029aa815740a325942b
Amandine Lee <amandine.m.lee@gmail.com>
2013-10-05 01:05:46 -0700

    SmartCell class and beginnings of a smarter strategy

--- sudoku_AI.py
+++ sudoku_AI.py
@@ -69,6 +69,55 @@ class DFSPlayer(object):
                     ret.append((i+1, j+1))
         return ret
 
+class SmarterPlayer(object):
+    
+    def __init__(self, b):
+        self.board = b
+        self.scells = [[SmartCell(b, i, j) for i in range(1,10)] \
+                for j in range(1,10)]
+        self.initial_dependencies() #Never sure whether to pass in members of self
+
+#TODO: Finish the box part
+    def initial_dependencies(self):
+        for rindex in range(9):
+            for cindex in range(9):
+                rowmates = zip([rindex]*8, [n for n in range(9)].remove(cindex))
+                colmates = zip([n for n in range(9)].remove(rindex), [cindex]*8)
+                boxrow = rindex//3
+                boxcol = cindex//3
+                boxmates = zip([
+                for rmate in rowmates.extend(colmates).extend(boxmates):
+                    row, col = rmate
+                    self.scells.dependencies.append(self.scells[row][col])
+#TODO: Test how far this gets without any further strategy
+
+#TODO: Develop further strategies. 
+
+
+
+class SmartCell(object):
+
+    def __init__(self, b, row, col):
+        self.board = b
+        self.row = row
+        self.col = col
+        self.possibilities = [i in range(1,10)]
+        self.dependencies = []
+
+    def remove_poss(self, num):
+        if num not in range(1,10):
+            raise SudokuValueError
+        else:
+            if num in self.possibilities:
+                self.possibilities.remove(num)
+            if len(self.possibilities == 1):
+                last = self.possibilities[0]
+                self.board.add_number(last, row, col)
+                for dep in self.dependencies:
+                    dep.remove_poss(last)
+
+
+
 if __name__ == '__main__':
     from sample_boards import *
     from sys import argv
